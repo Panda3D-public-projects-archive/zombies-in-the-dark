db7676fb642a255b85caee90a1224b8103760567
vznidar@gmail.com <vznidar@gmail.com@d8bc8831-9333-7e28-066f-d192a362f5b4>
2012-06-07 19:18:25 +0000

    -removed game implementation to game.py -added FSM

--- trunk/zitd/main.py
+++ trunk/zitd/main.py
@@ -7,56 +7,77 @@ from player import Player
 from utils import *
 from level import Level
 from monster import Monster
+from panda3d.rocket import *
+from gameui import *
+from game import *
+from direct.fsm import FSM#@UnresolvedImport
+from direct.task.Task import Task
 
 class App(DirectObject):
     def __init__(self):
         ShowBase()
+        # Instancing fsm
+        self.fsm = AppFSM(self, 'AppFSM')
+        
+        # Creating rocket region for ui
+        self.rRegion = RocketRegion.make('zitd_ui', base.win)
+        self.rContext = self.rRegion.getContext()
+        ih = RocketInputHandler()
+        base.mouseWatcher.attachNewNode(ih)
+        self.rRegion.setInputHandler(ih)
+        
+        self.acceptOnce('escape', self.pause)
+        
+        self.fsm.request('Menu')
+    
+    def pause(self):
+        self.fsm.request('Pause')
+        
+class AppFSM(FSM.FSM):
+    def __init__(self, parent, name):
+        FSM.FSM.__init__(self, name)
+        self.parent = parent
+
+        self.defaultTransitions = {
+            'Menu'       : ['NewGame'],
+            'NewGame'    : ['Pause'],
+            'Pause'      : ['ResumeGame', 'Menu'],
+            'ResumeGame' : ['Pause']
+            }
+    
+    def enterMenu(self):
         base.win.setClearColor(VBase4(0, 0, 0, 0))
+        # Instancing main menu UI
+        self.parent.menuui = MenuUI(self.parent) 
         
-        # GAMETYPE
-        self.type = 'DEBUG' # 'FPS' or 'DEBUG'
+    def exitMenu(self):
+        self.parent.menuui.cleanup()
         
-        # Creating level geometry
-        self.level = Level(self)
+    def enterNewGame(self):
+        self.parent.game = Game(self.parent)
+        # Instancing game UI
+        self.parent.gameui = GameUI(self.parent)
         
-        # Instance the player controller
-        self.player = Player(self, self.level.start_pos)
-        if self.type == 'FPS':
-            base.camera.reparentTo(self.player.node)
-            base.camLens.setFov(100)
-        elif self.type == 'DEBUG':
-            self.camera = CameraManager(self)
-        # Create player flashlight
-        self.slight = Spotlight('slight')
-        self.slight.setColor(VBase4(1, 1, 0.6, 1))
-        #self.lens = PerspectiveLens()
-        #self.slight.setLens(self.lens)
-        self.slnp = self.player.node.attachNewNode(self.slight)
-        self.slnp.node().getLens().setFov(55)
-        self.slnp.node().getLens().setNearFar(1, 10)
-        self.slight.setExponent(45)
-        self.slight.setAttenuation(Point3(0.737, 0.134, 0.001))
-        render.setLight(self.slnp)
+    def exitNewGame(self):
+        self.parent.gameui.cleanup()
         
-        # Instance one monster
-        self.baby = Monster(self, 'nos', (19,17))
+    def enterPause(self):
+        self.parent.game.pause()
+        # Instancing pause game UI
+        self.parent.pauseui = PauseUI(self.parent)
+        self.acceptOnce('escape', taskMgr.stop)
         
-        # Create ambient light
-        self.alight = AmbientLight("alight")
-        self.alnp = render.attachNewNode(self.alight)
-        render.setLight(self.alnp)
+    def exitPause(self):
+        self.acceptOnce('escape', self.parent.pause)
+        self.parent.pauseui.cleanup()
         
-        # Instance collision objects
-        self.pusher = CollisionHandlerFluidPusher()
-        self.pusher.addCollider(self.player.cn, self.player.node)
-        self.traverser = CollisionTraverser()
-        self.traverser.addCollider(self.player.cn, self.pusher)
-        self.traverser.setRespectPrevTransform(True) 
-        base.cTrav = self.traverser
-        #base.cTrav.showCollisions(render)
-        self.level.wall_node.setCollideMask(BitMask32.bit(0))
+    def enterResumeGame(self):
+        self.parent.game.resume()
+        # Instancing game UI
+        self.parent.gameui = GameUI(self.parent)
         
-        # Instance class for debug output
-        self.debug = DebugOptions(self)
+    def exitResumeGame(self):
+        self.parent.gameui.cleanup()
+
 a = App()
 run()
