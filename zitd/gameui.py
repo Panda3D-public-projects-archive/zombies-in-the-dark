7ef99c22af6b5f3d0adf72f9fdff6b3d06a8b4d3
vznidar@gmail.com <vznidar@gmail.com>
2012-06-10 09:27:21 +0000

    -finished implementation of ingame ui

--- zitd/gameui.py
+++ zitd/gameui.py
@@ -1,4 +1,4 @@
-from panda3d.core import TextNode, BitMask32, TextureStage, NodePath, DirectionalLight, AmbientLight, Vec4, Vec3, VBase4#@UnresolvedImport
+from panda3d.core import *#@UnresolvedImport
 from direct.gui.DirectGui import DirectButton, DirectEntry, DirectLabel#@UnresolvedImport
 from direct.gui.OnscreenText import OnscreenText#@UnresolvedImport
 from direct.gui.OnscreenImage import OnscreenImage
@@ -73,8 +73,59 @@ class GameUI():
         #context.LoadDocument('assets/background.rml').Show()
         
         self.doc = self.parent.rContext.LoadDocument('ui/gameui.rml')
+        inner_rml = ""
+        for x in xrange(0, self.parent.game.player.bullets):
+            inner_rml += '<div class="bullet"><div class="bullet_image"><img src="bullet.png" width="10px" height="29px"></img></div></div>'
+        
+        for x in xrange(self.parent.game.player.bullets, self.parent.game.player.max_bullets + 1):
+            inner_rml += '<div class="bullet"></div>'
+        
+        element = self.doc.GetElementById('top_right_container')
+        element.inner_rml = inner_rml
+        
+        element = self.doc.GetElementById('health_left')
+        element.style.width = str(self.parent.game.player.health * 1.5) + 'px'
+        
         self.doc.Show()
         
+    def minusHealth(self):
+        self.parent.game.player.health -= 1
+        if self.parent.game.player.health < 0:
+            self.parent.game.player.health = 0
+        self.refreshHealth()
+    
+    def plusHealth(self):
+        self.parent.game.player.health += 1
+        if self.parent.game.player.health > self.parent.game.player.max_health:
+            self.parent.game.player.health = self.parent.game.player.max_health
+        self.refreshHealth()
+        
+    def minusBullets(self):
+        self.parent.game.player.bullets -= 1
+        if self.parent.game.player.bullets < 0:
+            self.parent.game.player.bullets = 0
+        self.refreshBullets()
+    
+    def plusBullets(self):
+        self.parent.game.player.bullets += 1
+        if self.parent.game.player.bullets > self.parent.game.player.max_bullets:
+            self.parent.game.player.bullets = self.parent.game.player.max_bullets
+        self.refreshBullets()
+        
+    def refreshHealth(self):
+        element = self.doc.GetElementById('health_left')
+        element.style.width = str(self.parent.game.player.health * 1.5) + 'px'
+        
+    def refreshBullets(self):
+        inner_rml = ""
+        for x in xrange(0, self.parent.game.player.bullets):
+            inner_rml += '<div class="bullet"><div class="bullet_image"><img src="bullet.png" width="10px" height="29px"></img></div></div>'
+        
+        for x in xrange(self.parent.game.player.bullets, self.parent.game.player.max_bullets + 1):
+            inner_rml += '<div class="bullet"></div>'
+        
+        element = self.doc.GetElementById('top_right_container')
+        element.inner_rml = inner_rml
     
     def cleanup(self):
         self.parent.rRegion.setActive(0)
